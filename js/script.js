f2a2f77fe1fff72a63f03b9c980b059d8f88f7bf
FernandoAvellar <favellar@yahoo.com.br>
2016-03-31 10:05:56 -0300

    After lesson 59

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+
+
+//Used JQuery because bootstrap needs this, 
+//so pure JavaScript won´t work together bootstrap
+//$ represents the Jquery function

c0d4accb7bc955ec4165a9257711cea381a10372
FernandoAvellar <favellar@yahoo.com.br>
2016-03-31 14:41:30 -0300

    After Lesson 62

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //Collapse Menu Button Collapses the Menu when it loses focus
-
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
@@ -12,12 +11,13 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 });
 
 //Load the Main Menu inside SPA (Single Page Application) index.html
-
 (function (global) {
-
 var dc = {};
 
 var homeHtml = "snippets/home-snippet.html";
+var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
 
 //Convenience Function for inserting innerHTML for 'select'
 var insertHTML = function (selector, html) {
@@ -32,6 +32,13 @@ var showLoading = function (selector) {
 	insertHTML(selector, html);
 };
 
+//Return substitute of '{{propName}} with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+	 var propToReplace = "{{" + propName + "}}";
+	 string = string.replace(new RegExp(propToReplace, "g"), propValue);
+	 return string;
+}
+
 //On page Load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -45,6 +52,59 @@ $ajaxUtils.sendGetRequest(
 	},
 	false);
 });
+
+//Load the menu categories view
+dc.loadMenuCategories = function () {
+	 showLoading("#main-content");
+	 $ajaxUtils.sendGetRequest(
+	 	allCategoriesUrl,
+	 	builAndShowCategoriesHTML,
+	 	true); //true pra informar JASON e já converter pra string (parse)
+};
+
+//Builds HTML for the categories page based on the data from the server
+function builAndShowCategoriesHTML (categories) {
+	 //Load title snippet of categories page
+	 $ajaxUtils.sendGetRequest(
+	 	categoriesTitleHtml,
+	 	function (categoriesTitleHtml) {
+	 		//Retrieve single category snippet
+	 		$ajaxUtils.sendGetRequest(
+	 			categoryHtml,
+	 			function (categoryHtml) {
+	 				var categoriesViewHtml = 
+	 					builCategoriesViewHtml(categories,
+	 									       categoriesTitleHtml,
+	 										   categoryHtml);
+	 			insertHtml("#main-content", categoriesViewHtml);
+	 		},
+	 		false); //false to not process snippets as JASON
+	 	},
+	 	false);
+}
+
+//Using categories data and snippets html
+//build categories view HTML to be inserted into page
+function builCategoriesViewHtml (categories,
+								 categoriesTitleHtml,
+								 categoryHtml) {
+
+	var finalHtml = categoriesTitleHtml;
+	finalHtml += "<section clas='row'>";
+
+	//Loop over categories
+	for (var i = 0; i < categories.length; i++){
+		//Insert category values
+		var html = categoryHtml;
+		var name = "" + categories[i].name;
+		var short_name = categories[i].short_name;
+		html = insertProperty(html, "name", name);
+		html = insertProperty(html, "short_name", short_name);
+		finalHtml += html;
+	}
+	finalHtml += "</section>";
+	return finalHtml;	 
+}	
 
 global.$dc = dc;
 
